2c650692b08d1d58ab103724d658c8e317fafb92
reidrichardson <Lkr721993@gmail.com>
2014-04-11 01:15:25 -0400

    Uploaded first files
    Here are some prototypes

---
+++ src/nba.h
@@ -0,0 +1,21 @@
+#ifndef _NBA_H
+#define _NBA_H
+
+class NBA{
+	public:
+		getScore();
+		getName();
+		getTime();
+		getPeriod();
+		setScore();
+		setName();
+		setTime();
+		setPeriod();
+	private:
+		int score;
+		int time;
+		int period;		
+
+};
+
+#endif
